e95b812fd766c40b9c5eda3fc4b378557abc7227
Abir Aminul Islam <itsuriz.abir8@gmail.com>
2025-05-03 10:00:21 +0600

    Add GitHub Pages configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/personal_web' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/personal_web/' : '',
 };
 
 export default nextConfig;
